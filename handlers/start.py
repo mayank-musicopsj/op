13e6531e76b97dfe7963ab7d9460ae8748fc7f79
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-12 20:45:07 +0530

    Add files via upload

--- handlers/start.py
+++ handlers/start.py
@@ -6,11 +6,10 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(command("start") & filters.private & ~filters.group)
+@Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.delete()
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1ebc3393692680b98c7ef.jpg",
+        photo=f"https://telegra.ph/file/336544dbebf58dadb1e6f.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━
 💔 ʜᴇʏ {message.from_user.mention()} !
 
@@ -33,7 +32,7 @@ async def start_(client: Client, message: Message):
                        ),
                 ],[
                     InlineKeyboardButton(
-                        "•sᴏᴜʀᴄᴇ•", url=f"https://github.com/BANNA-XD143/Aaru_Music"
+                        " ❣ᴏᴡɴᴇʀ ❣ ", url=f"https://t.me/{me}"
                        ),
                     InlineKeyboardButton(
                         "•sᴜᴘᴘᴏʀᴛ•", url=f"https://t.me/AARU_SUPPORT"
